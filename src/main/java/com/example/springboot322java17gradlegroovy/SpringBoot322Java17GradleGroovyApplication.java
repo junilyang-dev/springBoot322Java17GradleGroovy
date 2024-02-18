47cf47ea8b67e4f31c4e0c308169342e9568be72
junil <junilyang.dev@gmail.com>
2024-02-18 19:10:19 +0900

    springBoot 3.2.2 java 17 gradle groovy dependencies(Spring Web, H2 Database, Mustache, Spring DAta JPA)

---
+++ src/main/java/com/example/springboot322java17gradlegroovy/SpringBoot322Java17GradleGroovyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot322java17gradlegroovy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot322Java17GradleGroovyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot322Java17GradleGroovyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot322java17gradlegroovy/SpringBoot322Java17GradleGroovyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot322java17gradlegroovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot322Java17GradleGroovyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
