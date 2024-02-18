60053ff0039f8b96c303c9efa235adaf1e8e3282
junil <junilyang.dev@gmail.com>
2024-02-18 20:07:11 +0900

    mustache model addAttribute username

--- src/main/java/com/example/springboot322java17gradlegroovy/controller/FirstController.java
+++ src/main/java/com/example/springboot322java17gradlegroovy/controller/FirstController.java
@@ -1,12 +1,14 @@
 package com.example.springboot322java17gradlegroovy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/hi")
-    public String niceToMeetYou(){
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username","준일");
         return "greetings";//templates/greetings.mustache
     }
 }
