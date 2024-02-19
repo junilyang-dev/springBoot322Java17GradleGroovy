40a6262b0dbb9a3e185a18194a68d46c816eb277
junil <junilyang.dev@gmail.com>
2024-02-19 15:54:58 +0900

    mustache model addAttribute nickname

--- src/main/java/com/example/springboot322java17gradlegroovy/controller/FirstController.java
+++ src/main/java/com/example/springboot322java17gradlegroovy/controller/FirstController.java
@@ -11,4 +11,10 @@ public class FirstController {
         model.addAttribute("username","준일");
         return "greetings";//templates/greetings.mustache
     }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname","홍길동");
+        return "goodbye";
+    }
 }
