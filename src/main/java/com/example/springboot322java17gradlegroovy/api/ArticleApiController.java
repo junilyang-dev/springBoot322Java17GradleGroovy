2a5b5d3a14bebdce36691698caec4177fd8363a2
junil <junilyang.dev@gmail.com>
2024-03-16 20:48:04 +0900

    GET 구현 - @GetMapping, @Autowired, DI

---
+++ src/main/java/com/example/springboot322java17gradlegroovy/api/ArticleApiController.java
@@ -0,0 +1,30 @@
+package com.example.springboot322java17gradlegroovy.api;
+
+import com.example.springboot322java17gradlegroovy.entity.Article;
+import com.example.springboot322java17gradlegroovy.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ArticleApiController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    // GET
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+    // POST
+    // PATCH
+    // DELETE
+}
