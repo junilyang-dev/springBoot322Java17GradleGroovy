c083659942bef314ecce524086c2862d1fc43dec
junil <junilyang.dev@gmail.com>
2024-08-29 22:15:44 +0900

    mysql, jwt, openapi(swagger), javax 의존성 추가

--- src/main/java/com/example/springboot322java17gradlegroovy/SpringBoot322Java17GradleGroovyApplication.java
+++ src/main/java/com/example/springboot322java17gradlegroovy/SpringBoot322Java17GradleGroovyApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBoot322Java17GradleGroovyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot322Java17GradleGroovyApplication.class, args);
+        System.out.println("Hello World!");
     }
 }
