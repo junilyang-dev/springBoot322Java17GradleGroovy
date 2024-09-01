88cf7ed9997fa9c37a7f8cf10e4b549fa3b392d8
junil <junilyang.dev@gmail.com>
2024-09-01 15:43:43 +0900

    로그인 null check

--- src/main/java/com/example/springboot322java17gradlegroovy/user/UserController.java
+++ src/main/java/com/example/springboot322java17gradlegroovy/user/UserController.java
@@ -2,10 +2,15 @@ package com.example.springboot322java17gradlegroovy.user;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -15,12 +20,24 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/login")
-    public UserEntity login(
+    public ResponseEntity<Map<String, Object>> login(
             @RequestParam("userId") String userId,
             @RequestParam("password") String password
     ) {
-        UserEntity userEntity = new UserEntity();
-        userEntity = userService.loginUsers(userId, password);
-        return userEntity;
+        Map<String, Object> resultMap = new HashMap<>();
+        if(userId == null) {
+            resultMap.put("success", false);
+            resultMap.put("message", "사용자ID를 입력해주세요.");
+            resultMap.put("data", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
+        }
+        if(password == null) {
+            resultMap.put("success", false);
+            resultMap.put("message", "비밀번호를 입력해주세요.");
+            resultMap.put("data", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
+        }
+        resultMap = userService.loginUsers(userId, password);
+        return ResponseEntity.ok(resultMap);
     }
 }
