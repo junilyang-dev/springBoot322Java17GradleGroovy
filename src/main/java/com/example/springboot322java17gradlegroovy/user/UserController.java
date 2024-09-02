9cc9a991087811d674c786d4b6604e3a126dee10
junil <junilyang.dev@gmail.com>
2024-09-02 22:26:36 +0900

    토큰 검증, 접속 IP 및 디바이스 확인

--- src/main/java/com/example/springboot322java17gradlegroovy/user/UserController.java
+++ src/main/java/com/example/springboot322java17gradlegroovy/user/UserController.java
@@ -1,5 +1,7 @@
 package com.example.springboot322java17gradlegroovy.user;
 
+import com.example.springboot322java17gradlegroovy.config.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,11 +20,42 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<Map<String, Object>> login(
-            @RequestBody LoginRequest loginRequest
+            @RequestBody LoginRequest loginRequest,
+            HttpServletRequest request
     ) {
+        //접속 IP
+        String clientIp = request.getRemoteAddr();
+        //접속 디바이스 시작
+        String userAgent = request.getHeader("User-Agent");
+        String device = "";
+        if (userAgent == null) {
+            device = "Unknown";
+        }
+
+        if (userAgent.contains("Windows")) {
+            device = "Windows PC";
+        } else if (userAgent.contains("iPhone")) {
+            device = "iPhone";
+        } else if (userAgent.contains("iPad")) {
+            device = "iPad";
+        } else if (userAgent.contains("Mac OS")) {
+            device = "Mac PC";
+        } else if (userAgent.contains("Android")) {
+            if (userAgent.contains("Mobile")) {
+                device = "Android Phone";
+            } else {
+                device = "Android Tablet";
+            }
+        } else {
+            device = "Unknown Device";
+        }
+        //접속 디바이스 끝
+        //결과값을 담을 Map 생성
         Map<String, Object> resultMap = new HashMap<>();
+        //ResponseBody에서 데이터 호출
         String userId = loginRequest.getUserId();
         String password = loginRequest.getPassword();
+        //요청값 검증 시작
         if(userId == null) {
             resultMap.put("success", false);
             resultMap.put("message", "사용자ID를 입력해주세요.");
@@ -35,7 +68,27 @@ public class UserController {
             resultMap.put("data", "");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
         }
-        resultMap = userService.loginUsers(userId, password);
+        //요청값 검증 끝
+        //로그인 서비스 호출
+        resultMap = userService.loginUsers(userId, password, clientIp);
         return ResponseEntity.ok(resultMap);
+    }
+
+    @GetMapping("/test")
+    public String test(HttpServletRequest request) {
+        Map<String, Object> resultMap = new HashMap<>();
+        // JWT 토큰 검증 시작
+        String authorizationHeader = request.getHeader("Authorization"); // 요청 헤더에서 Authorization 헤더 값을 가져옴
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) { // Authorization 헤더가 없거나 Bearer로 시작하지 않으면
+            return "";
+        }
+        String jwtToken = authorizationHeader.substring(7); // "Bearer " 다음의 토큰 부분만 추출
+        boolean tokenChk = JwtUtil.validateToken(jwtToken);
+        if(tokenChk) {
+            String userId = JwtUtil.getUserIdFromToken(jwtToken); // 토큰에서 사용자 ID를 추출
+            return userId;
+        }else{
+            return "토큰 만료";
+        }
     }
 }
