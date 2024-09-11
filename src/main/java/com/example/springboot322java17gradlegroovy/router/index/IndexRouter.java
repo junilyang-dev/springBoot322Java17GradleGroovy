f7020660ba995df0ed9ed66f6602df0502b0d586
junil <junilyang.dev@gmail.com>
2024-09-11 20:29:25 +0900

    pageTitle 수정

--- src/main/java/com/example/springboot322java17gradlegroovy/router/index/IndexRouter.java
+++ src/main/java/com/example/springboot322java17gradlegroovy/router/index/IndexRouter.java
@@ -1,12 +1,15 @@
 package com.example.springboot322java17gradlegroovy.router.index;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class IndexRouter {
     @GetMapping(value = {"/","/index"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("pageTitle", "Index Page");
         return "index/index";//templates/index/index.mustache
     }
 }
--- src/main/java/com/example/springboot322java17gradlegroovy/router/login/LoginRouter.java
+++ src/main/java/com/example/springboot322java17gradlegroovy/router/login/LoginRouter.java
@@ -1,18 +1,21 @@
 package com.example.springboot322java17gradlegroovy.router.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginRouter {
 
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model){
+        model.addAttribute("pageTitle", "Login Page");
         return "login/login";//templates/login/login.mustache
     }
 
     @GetMapping("/join")
-    public String join() {
+    public String join(Model model) {
+        model.addAttribute("pageTitle", "Join Page");
         return "login/join";//templates/login/join.mustache
     }
 }
