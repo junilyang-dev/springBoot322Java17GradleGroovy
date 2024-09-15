740ec8ae96ccb88f994f12e4982ebcd179e65e0d
junil <junilyang.dev@gmail.com>
2024-09-15 20:47:21 +0900

    토스트 색상변경 파라메터 수정

--- src/main/resources/static/js/utility/toast.js
+++ src/main/resources/static/js/utility/toast.js
@@ -1,6 +1,6 @@
-function showToast(message) {
-    const toastMessage = document.getElementById('toastMessage');
-    toastMessage.textContent = message;
-    const toastElement = new bootstrap.Toast(document.getElementById('customToast'));
-    toastElement.show();
+function showToast(message, type) {
+    $('#customToast').removeClass().addClass(`toast ${type ?? ''}`);
+    $('#customToast strong').removeClass().addClass(`me-auto ${type ?? ''}`);
+    $('#toastMessage').text(message);
+    new bootstrap.Toast($('#customToast')).show();
 }
