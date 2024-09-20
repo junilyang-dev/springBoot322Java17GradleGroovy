57060f5c2bc0c20871057f30ad2749f1d7ec4c2b
junil <junilyang.dev@gmail.com>
2024-09-20 21:02:20 +0900

    검증 오류 전역 예외처리

---
+++ src/main/java/com/example/springboot322java17gradlegroovy/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.springboot322java17gradlegroovy.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> resultMap = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            resultMap.put("success", false);
+            resultMap.put("message", errorMessage);
+            resultMap.put("data", fieldName);
+        });
+        return new ResponseEntity<>(resultMap, HttpStatus.BAD_REQUEST);
+    }
+}
